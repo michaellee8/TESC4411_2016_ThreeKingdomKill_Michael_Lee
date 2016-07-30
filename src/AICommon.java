b6f919d7f4459b36c2509c20db881b746d6b2ee1
michaellee8 <ckmichael8@gmail.com>
2016-07-30 20:21:18 +0800

    Edit AICommon.java

--- src/AICommon.java
+++ src/AICommon.java
@@ -1,36 +1,46 @@
 import comp.*;
 
 public abstract class AICommon extends Hero {
-    public abstract void join(int index);
+	public abstract void join(int index);
 
-    public abstract void beforeMyTurn();
+	protected void beforeMyTurn() {
 
-    protected static Card[] CardDiscardSequence = {Card.Repeal, Card.Grab, Card.Agility, Card.Axe, Card.Shield, Card.Vitality, Card.Attack,
-            Card.Defense, Card.Heal             };
+	}
 
-    public void drawing() {
-        draw();
-    }
+	protected void afterMyTurn() {
 
-    public void myTurn() {
-        while (countHandCards(Card.Grab) > 0) {
-            if (GameMaster.getInstance().countOppBuffs() > 0) {
+	}
 
-            }
-        }
-    }
+	protected static Card[] CardDiscardSequence = { Card.Repeal, Card.Grab, Card.Agility, Card.Axe, Card.Shield,
+			Card.Vitality, Card.Attack, Card.Defense, Card.Heal };
 
-    public void discarding() {
-        for (int i = 0; i < CardDiscardSequence.length; i++) {
-            while (countHandCards(CardDiscardSequence[i]) > 0) {
-                discard(CardDiscardSequence[i]);
-            }
-        }
-    }
+	public void drawing() {
+		draw();
+	}
 
-    public void beingAttack() {
-        if (countHandCards(Card.Defense) > 0) {
-            use(Card.Defense);
-        }
-    }
+	public void myTurn() {
+		beforeMyTurn();
+		while (countHandCards(Card.Grab) > 0 && this.countBuffs() < 2) {
+			if (GameMaster.getInstance().countOppBuffs() > 0) {
+				Card[] c = GameMaster.getInstance().getOppBuffList();
+			} else {
+
+			}
+		}
+		afterMyTurn();
+	}
+
+	public void discarding() {
+		for (int i = 0; i < CardDiscardSequence.length; i++) {
+			while (countHandCards(CardDiscardSequence[i]) > 0) {
+				discard(CardDiscardSequence[i]);
+			}
+		}
+	}
+
+	public void beingAttack() {
+		if (countHandCards(Card.Defense) > 0) {
+			use(Card.Defense);
+		}
+	}
 }
