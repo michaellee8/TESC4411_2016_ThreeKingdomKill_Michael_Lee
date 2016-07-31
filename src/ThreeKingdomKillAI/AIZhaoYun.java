4405c176a01b26c288d751ab11e050b196edaee0
Michael Lee <ckmichael8@gmail.com>
2016-07-31 07:31:25 +0000

    Finish AIZhaoYun

---
+++ src/ThreeKingdomKillAI/AIZhaoYun.java
@@ -0,0 +1,33 @@
+package ThreeKingdomKillAI;
+
+import comp.*;
+
+public class AIZhaoYun extends AICommon {
+    public AIZhaoYun() {
+
+    }
+
+    public final void join(int index) {
+        GameMaster master = GameMaster.getInstance();
+        GameMaster.Player player = master.players[index];
+        player.attList.add(new Attributes("趙雲", 400, 1, 2, 0, 0, null));
+        player.heroes.add(this);
+    }
+
+    @Override
+    protected Boolean canDefense() {
+        return (this.countHandCards(Card.Defense) + this.countHandCards(Card.Attack)) > 0;
+    }
+
+    @Override
+    protected Boolean canAttack() {
+        if (this.getHp() / this.AgressiveCoefficient / 1.5 <= this.getMaxHp()
+            && (this.countHandCards(Card.Defense) + this.countHandCards(Card.Attack)) < 2) {
+            return false;
+        } else if ((this.countHandCards(Card.Defense) + this.countHandCards(Card.Attack)) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
